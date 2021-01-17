1498f4dd61b1de449a1dda02a39bd30c4e2be1c9
Pinakee15 <iampinakee2098@gmail.com>
2021-01-17 23:05:23 +0530

    Removed the first card from the main card components

--- app/src/Components/NewsCards/NewsCards.js
+++ app/src/Components/NewsCards/NewsCards.js
@@ -6,19 +6,19 @@ import useStyles from './Styles';
 const NewsCards = ({articles}) => {
     const classes = useStyles();
     const infoCards = [
-        { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' , bgColor : "linear-gradient(to bottom right, rgb(249, 128, 130,5), rgb(61, 158, 250,4))" },
+        // { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' , bgColor : "linear-gradient(to bottom right, rgb(249, 128, 130,5), rgb(61, 158, 250,4))" },
         { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news', bgColor : "linear-gradient(to bottom right, rgba(250, 246, 30, 0.7), rgba(231, 78, 13, 0.8))" },
         { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5',bgColor : "linear-gradient(to bottom right, rgba(32, 186, 217, 0.8), #0080ff" },
         { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN',bgColor : "linear-gradient(to bottom right, rgba(199, 246, 90, 0.8), rgba(55, 77, 4, 0.8))" },
       ];
-
+    console.log(articles); 
     if (!articles.length){
         return (
             <Grow in>
                 <Grid className={classes.container} container alignItems="stretch" spacing = {3} >
                     {
                         infoCards.map(infoCard=>(
-                        < Grid item xs = {12} sm={6} md ={4} lg={3} className = {classes.infoCard}> 
+                        < Grid item xs = {12} sm={12} md ={4} lg={4} className = {classes.infoCard}> 
                             <div className = {classes.card} style = {{backgroundImage:infoCard.bgColor}}>
                                 <Typography variant = "h5">{infoCard.title}</Typography>
                                 { infoCard.info ? 
